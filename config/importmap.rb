2cc427fd16d05fbedb3b7c3c78425b92b1967ebb
marat <mf.rakhimov1982@gmail.com>
2024-03-14 20:23:19 +0300

    Add Bootstrap

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,3 +8,5 @@ pin '@hotwired/stimulus', to: 'stimulus.min.js'
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js'
 pin_all_from 'app/javascript/controllers', under: 'controllers'
 pin 'flatpickr' # @4.6.13
+pin "bootstrap" # @5.3.3
+pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
