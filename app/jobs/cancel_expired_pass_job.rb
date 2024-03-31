0b8a59d29203ed66bf2c7cb7fe2418b7d07bd466
marat <mf.rakhimov1982@gmail.com>
2024-03-31 22:52:38 +0300

    Update background job for expired passes

--- app/jobs/cancel_expired_pass_job.rb
+++ app/jobs/cancel_expired_pass_job.rb
@@ -2,6 +2,6 @@ class CancelExpiredPassJob < ApplicationJob
   queue_as :default
 
   def perform
-    Pass.where("visit_date + INTERVAL '1 day' < ?", Time.now).update_all(status: :canceled)
+    Pass.where("visit_date + INTERVAL '1 day' < ?", Time.now).where.not(status: :finished).update_all(status: :canceled)
   end
 end
