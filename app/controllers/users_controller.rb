c13552193453f6602445736cd2a46af6f8853c5c
marat <mf.rakhimov1982@gmail.com>
2024-04-04 21:09:32 +0300

    Add 404 rendering for User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,6 +34,7 @@ class UsersController < ApplicationController
   end
 
   def find_user
-    @user = User.find(params[:id])
+    @user = User.find_by(id: params[:id])
+    render_404 unless @user
   end
 end
